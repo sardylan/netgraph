c1d6ab9b29699f12090399b73a9685a95951529f
Luca Cireddu <luca@lucacireddu.it>
2013-07-27 20:09:17 +0200

    Added BANDWIDTH and TIME text

--- imagefoo.c
+++ imagefoo.c
@@ -34,6 +34,8 @@ gdImagePtr ngImageCreate(int x, int y)
     int graph_y;
     int gray_value, color_gradient;
     int border_left, border_right, border_top, border_bottom;
+    int vertical_baseline, vertical_shift;
+    int horizontal_baseline, horizontal_shift;
 
     border_left = NETGRAPH_CONFIG_IMAGE_BORDER_LEFT_DEFAULT;
     border_right = NETGRAPH_CONFIG_IMAGE_BORDER_RIGHT_DEFAULT;
@@ -55,6 +57,131 @@ gdImagePtr ngImageCreate(int x, int y)
 
     color_black = gdImageColorAllocateAlpha(img, 0, 0, 0, 0);
     gdImageRectangle(img, border_left, border_top, x-border_right, y-border_bottom, color_black);
+
+    vertical_baseline = border_left - 5;
+    horizontal_baseline = y-border_bottom + 9;
+
+    // B
+    vertical_shift = y - border_bottom;
+
+    gdImageLine(img, vertical_baseline-4, vertical_shift, vertical_baseline, vertical_shift, color_black);
+    gdImageSetPixel(img, vertical_baseline, vertical_shift-1, color_black);
+    gdImageSetPixel(img, vertical_baseline-2, vertical_shift-1, color_black);
+    gdImageSetPixel(img, vertical_baseline-4, vertical_shift-1, color_black);
+    gdImageSetPixel(img, vertical_baseline, vertical_shift-2, color_black);
+    gdImageSetPixel(img, vertical_baseline-2, vertical_shift-2, color_black);
+    gdImageSetPixel(img, vertical_baseline-4, vertical_shift-2, color_black);
+    gdImageSetPixel(img, vertical_baseline-1, vertical_shift-3, color_black);
+    gdImageSetPixel(img, vertical_baseline-3, vertical_shift-3, color_black);
+
+    // A
+    vertical_shift = y - border_bottom - 5;
+
+    gdImageLine(img, vertical_baseline-3, vertical_shift, vertical_baseline, vertical_shift, color_black);
+    gdImageLine(img, vertical_baseline-3, vertical_shift-3, vertical_baseline, vertical_shift-3, color_black);
+    gdImageSetPixel(img, vertical_baseline-2, vertical_shift-1, color_black);
+    gdImageSetPixel(img, vertical_baseline-4, vertical_shift-1, color_black);
+    gdImageSetPixel(img, vertical_baseline-2, vertical_shift-2, color_black);
+    gdImageSetPixel(img, vertical_baseline-4, vertical_shift-2, color_black);
+
+    // N
+    vertical_shift = y - border_bottom - 10;
+
+    gdImageLine(img, vertical_baseline-4, vertical_shift, vertical_baseline, vertical_shift, color_black);
+    gdImageLine(img, vertical_baseline-4, vertical_shift-3, vertical_baseline, vertical_shift-3, color_black);
+    gdImageSetPixel(img, vertical_baseline-3, vertical_shift-1, color_black);
+    gdImageSetPixel(img, vertical_baseline-2, vertical_shift-1, color_black);
+    gdImageSetPixel(img, vertical_baseline-2, vertical_shift-2, color_black);
+    gdImageSetPixel(img, vertical_baseline-1, vertical_shift-2, color_black);
+
+    // D
+    vertical_shift = y - border_bottom - 15;
+
+    gdImageLine(img, vertical_baseline-4, vertical_shift, vertical_baseline, vertical_shift, color_black);
+    gdImageSetPixel(img, vertical_baseline, vertical_shift-1, color_black);
+    gdImageSetPixel(img, vertical_baseline-4, vertical_shift-1, color_black);
+    gdImageSetPixel(img, vertical_baseline, vertical_shift-2, color_black);
+    gdImageSetPixel(img, vertical_baseline-4, vertical_shift-2, color_black);
+    gdImageSetPixel(img, vertical_baseline-1, vertical_shift-3, color_black);
+    gdImageSetPixel(img, vertical_baseline-2, vertical_shift-3, color_black);
+    gdImageSetPixel(img, vertical_baseline-3, vertical_shift-3, color_black);
+
+    // W
+    vertical_shift = y - border_bottom - 20;
+
+    gdImageLine(img, vertical_baseline-4, vertical_shift, vertical_baseline-1, vertical_shift, color_black);
+    gdImageLine(img, vertical_baseline-2, vertical_shift-1, vertical_baseline, vertical_shift-1, color_black);
+    gdImageLine(img, vertical_baseline-3, vertical_shift-2, vertical_baseline-1, vertical_shift-2, color_black);
+    gdImageLine(img, vertical_baseline-2, vertical_shift-3, vertical_baseline, vertical_shift-3, color_black);
+    gdImageLine(img, vertical_baseline-4, vertical_shift-4, vertical_baseline-1, vertical_shift-4, color_black);
+
+    // I
+    vertical_shift = y - border_bottom - 24;
+
+    gdImageLine(img, vertical_baseline-4, vertical_shift-2, vertical_baseline, vertical_shift-2, color_black);
+
+    // D
+    vertical_shift = y - border_bottom - 28;
+
+    gdImageLine(img, vertical_baseline-4, vertical_shift, vertical_baseline, vertical_shift, color_black);
+    gdImageSetPixel(img, vertical_baseline, vertical_shift-1, color_black);
+    gdImageSetPixel(img, vertical_baseline-4, vertical_shift-1, color_black);
+    gdImageSetPixel(img, vertical_baseline, vertical_shift-2, color_black);
+    gdImageSetPixel(img, vertical_baseline-4, vertical_shift-2, color_black);
+    gdImageSetPixel(img, vertical_baseline-1, vertical_shift-3, color_black);
+    gdImageSetPixel(img, vertical_baseline-2, vertical_shift-3, color_black);
+    gdImageSetPixel(img, vertical_baseline-3, vertical_shift-3, color_black);
+
+    // T
+    vertical_shift = y - border_bottom - 33;
+
+    gdImageLine(img, vertical_baseline-4, vertical_shift-1, vertical_baseline, vertical_shift-1, color_black);
+    gdImageSetPixel(img, vertical_baseline-4, vertical_shift, color_black);
+    gdImageSetPixel(img, vertical_baseline-4, vertical_shift-2, color_black);
+
+    // H
+    vertical_shift = y - border_bottom - 37;
+
+    gdImageLine(img, vertical_baseline-4, vertical_shift, vertical_baseline, vertical_shift, color_black);
+    gdImageLine(img, vertical_baseline-4, vertical_shift-3, vertical_baseline, vertical_shift-3, color_black);
+    gdImageSetPixel(img, vertical_baseline-2, vertical_shift-1, color_black);
+    gdImageSetPixel(img, vertical_baseline-2, vertical_shift-2, color_black);
+
+
+    // T
+    horizontal_shift = border_left;
+
+    gdImageLine(img, horizontal_shift+1, horizontal_baseline-4, horizontal_shift+1, horizontal_baseline, color_black);
+    gdImageSetPixel(img, horizontal_shift, horizontal_baseline-4, color_black);
+    gdImageSetPixel(img, horizontal_shift+2, horizontal_baseline-4, color_black);
+
+    // I
+    horizontal_shift = border_left + 4;
+
+    gdImageLine(img, horizontal_shift, horizontal_baseline-4, horizontal_shift, horizontal_baseline, color_black);
+
+    // M
+    horizontal_shift = border_left + 6;
+
+    gdImageLine(img, horizontal_shift, horizontal_baseline-4, horizontal_shift, horizontal_baseline, color_black);
+    gdImageLine(img, horizontal_shift+4, horizontal_baseline-4, horizontal_shift+4, horizontal_baseline, color_black);
+    gdImageSetPixel(img, horizontal_shift+1, horizontal_baseline-4, color_black);
+    gdImageSetPixel(img, horizontal_shift+1, horizontal_baseline-3, color_black);
+    gdImageSetPixel(img, horizontal_shift+2, horizontal_baseline-3, color_black);
+    gdImageSetPixel(img, horizontal_shift+2, horizontal_baseline-2, color_black);
+    gdImageSetPixel(img, horizontal_shift+3, horizontal_baseline-4, color_black);
+    gdImageSetPixel(img, horizontal_shift+3, horizontal_baseline-3, color_black);
+
+    // E
+    horizontal_shift = border_left + 12;
+
+    gdImageLine(img, horizontal_shift, horizontal_baseline-4, horizontal_shift, horizontal_baseline, color_black);
+    gdImageSetPixel(img, horizontal_shift+1, horizontal_baseline-4, color_black);
+    gdImageSetPixel(img, horizontal_shift+2, horizontal_baseline-4, color_black);
+    gdImageSetPixel(img, horizontal_shift+1, horizontal_baseline-2, color_black);
+    gdImageSetPixel(img, horizontal_shift+2, horizontal_baseline-2, color_black);
+    gdImageSetPixel(img, horizontal_shift+1, horizontal_baseline, color_black);
+    gdImageSetPixel(img, horizontal_shift+2, horizontal_baseline, color_black);
 
     return img;
 }
